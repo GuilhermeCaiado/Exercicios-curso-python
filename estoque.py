f6c932b90d327b5bf40cb85b462ccc98d15e1a47
Guilherme Caiado Richa <161043596+GuilhermeCaiado@users.noreply.github.com>
2025-09-23 09:44:16 -0300

    Estoque.py
    Fiz esse programa de estoque para treinar tratamento de listas

---
+++ estoque.py
@@ -0,0 +1,64 @@
+produtos = []
+quantidade = []
+precos = []
+
+#MENU
+while True: 
+
+    print ("-----------Menu----------")
+    print (" 1 - Adicionar produto \n 2 - Listar produto \n 3 - Vender produto \n 4 - Relatório de estoque \n 5 - Sair")
+    menus = int(input("Informe o que você quer fazer: "))
+    print ("\n")
+
+    if menus == 1:
+        pergunta_produto = str(input("Informe o nome do produto: ").lower())
+        produtos.append(pergunta_produto)
+
+        pergunta_quantidade = int(input("Informe a quantidade desse produto: "))
+        quantidade.append(pergunta_quantidade)
+
+        pergunta_preco = float(input("Informe o preço desse produto. (Ex: R$: 4.50): R$ "))
+        precos.append(pergunta_preco)
+    
+    elif menus == 2:
+        contador_indice = 0
+        while contador_indice < len(produtos):
+            print(f"Produto: {produtos[contador_indice]}")
+            print(f"Quantidade: {quantidade[contador_indice]}")
+            print(f"Preço: R${precos[contador_indice]:.2f} \n")
+            contador_indice += 1
+    
+    elif menus == 3:
+
+        print("----------VENDA----------")
+        pergunta_venda_nome = str(input("Informe o nome do produto: ").lower())
+        
+        try:
+            indice_do_produto = produtos.index(pergunta_venda_nome.lower()) 
+        except ValueError:
+            print(f"O produto '{pergunta_venda_nome}' não foi encontrado na lista.")
+
+        pergunta_venda_quantidade = int(input("Informe a quantidade que vai ser vendida: "))
+        
+        if quantidade[indice_do_produto] >= pergunta_venda_quantidade:
+            estoque = quantidade[indice_do_produto] - pergunta_venda_quantidade
+            quantidade[indice_do_produto] = estoque
+        else:
+            print(f"Quantidade acima do estoque atual!!! \nEstoque atual: {quantidade[indice_do_produto]}")
+
+        preco_venda = pergunta_venda_quantidade * precos[indice_do_produto]
+        print(f"VALOR DA VENDA: {preco_venda} \nVENDA REALIZADA!!!!")
+    
+    elif menus == 4:
+        quantidade_total = len(produtos)
+        print (f"Você tem {quantidade_total} produtos cadastrados.")
+
+        soma_relatorio = sum(quantidade) * sum(precos)
+        print(f"O valor do seu estoque é: {soma_relatorio}")
+
+    elif menus == 5:
+        print("OPERAÇÂO FECHADA!!!!")
+        break
+    
+    else:
+        print("Informe um valor válido: ")
