3c7bc19b9e08eda178e7a0168fcc9d835c63328e
Guilherme Caiado Richa <161043596+GuilhermeCaiado@users.noreply.github.com>
2025-09-23 10:15:06 -0300

    Estoque 1.1
    Foi adicionado coisas que eu esqueci, para o código ficar mais limpo.

--- estoque.py
+++ estoque.py
@@ -1,3 +1,12 @@
+import os
+
+def limpar_tela():
+  if os.name == 'nt':
+     _ = os.system('cls')
+  else:
+     _ = os.system('clear')
+
+
 produtos = []
 quantidade = []
 precos = []
@@ -10,6 +19,7 @@ while True:
     menus = int(input("Informe o que você quer fazer: "))
     print ("\n")
 
+    #ADICIONAR PRODUTOS
     if menus == 1:
         pergunta_produto = str(input("Informe o nome do produto: ").lower())
         produtos.append(pergunta_produto)
@@ -19,8 +29,11 @@ while True:
 
         pergunta_preco = float(input("Informe o preço desse produto. (Ex: R$: 4.50): R$ "))
         precos.append(pergunta_preco)
+        limpar_tela()
     
+    #LISTAR PRODUTOS
     elif menus == 2:
+        limpar_tela()
         contador_indice = 0
         while contador_indice < len(produtos):
             print(f"Produto: {produtos[contador_indice]}")
@@ -28,8 +41,9 @@ while True:
             print(f"Preço: R${precos[contador_indice]:.2f} \n")
             contador_indice += 1
     
+    #VENDER PRODUTOS
     elif menus == 3:
-
+        limpar_tela()
         print("----------VENDA----------")
         pergunta_venda_nome = str(input("Informe o nome do produto: ").lower())
         
@@ -49,16 +63,23 @@ while True:
         preco_venda = pergunta_venda_quantidade * precos[indice_do_produto]
         print(f"VALOR DA VENDA: {preco_venda} \nVENDA REALIZADA!!!!")
     
+    #RELATORIO DE ESTOQUE
     elif menus == 4:
+        limpar_tela()
         quantidade_total = len(produtos)
         print (f"Você tem {quantidade_total} produtos cadastrados.")
 
         soma_relatorio = sum(quantidade) * sum(precos)
         print(f"O valor do seu estoque é: {soma_relatorio}")
 
+    #SAIR
     elif menus == 5:
+        limpar_tela()
         print("OPERAÇÂO FECHADA!!!!")
         break
     
     else:
+        limpar_tela()
         print("Informe um valor válido: ")
+    
+    
